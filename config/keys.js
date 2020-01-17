dd1b6eafc63d39a875c549ab66b42d3068b06d97
stevelagarto <stevenzgz@gmail.com>
2020-01-17 12:58:15 +0100

    test database created

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,10 @@
 if (process.env.NODE_ENV === 'production') {
+  console.log('IN PRODUCTION ENV');  
   module.exports = require('./prod');
-}  {
+} else if (process.env.NODE_ENV === 'test') {
+  console.log('IN TEST ENV');
+  module.exports = require('./test.js');
+} else {
+  console.log('IN DEV ENV');
   module.exports = require('./dev');
 }
